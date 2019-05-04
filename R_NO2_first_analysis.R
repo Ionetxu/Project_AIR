a8a7ea7f337240be631af49286dada80bb09a6f6
Ione <ionelertxundi@hotmail.com>
2019-05-04 19:14:05 +0200

    Update

--- R_NO2_first_analysis.R
+++ R_NO2_first_analysis.R
@@ -102,132 +102,109 @@ Observ_fabra_NO2 <- airNO2_1 %>% filter(station_code == 58)
 
 #Let's do some initial plots by station:
 #St Gervasi 
-ggplot(St_gervasi_NO2, aes(x = dt, y = value)) + 
+St_Gervasi_NO2_plt <- ggplot(St_gervasi_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - St Gervasi")
 
 #Only data from 1991 to 1997 - not interesting?
 
 #Poblenou
-ggplot(Poblenou_NO2, aes(x = dt, y = value)) + 
+Poblenou_NO2_plt <- ggplot(Poblenou_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Poblenou")
 #Good data from 1991 to 2019, with breaks in between
 
 #Sagrera
-ggplot(Sagrera_NO2, aes(x = dt, y = value)) + 
+Sagrera_NO2_plt <- ggplot(Sagrera_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2 (µg/m3)- Sagrera")
 #Only data from 1993 to 2002
 
 #Sants
-ggplot(Sants_NO2, aes(x = dt, y = value)) + 
+Sants_NO2_plt <- ggplot(Sants_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Sants")
 #Data from 1995 to 2019
 #Eixample
-ggplot(Eixample_NO2, aes(x = dt, y = value)) + 
+Eixample_NO2_plt <- ggplot(Eixample_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Eixample")
 #Data from 1995 to 2019
 #Gracia
-ggplot(Gracia_NO2, aes(x = dt, y = value)) + 
+Gracia_NO2_plt <- ggplot(Gracia_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Gracia")
 #Good data from 1995 to 2019
 #Ciutatella
-ggplot(Ciutatella_NO2, aes(x = dt, y = value)) + 
+Ciutatella_NO2_plt <- ggplot(Ciutatella_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Ciutatella")
 #Data from 2004 to 2019
 
 #Torre_girona
-ggplot(Torre_girona_NO2, aes(x = dt, y = value)) + 
+Torre_girona_NO2_plt <- ggplot(Torre_girona_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Torre Girona")
 #Data from 2006 to 2019
 #Vall_hebron
-ggplot(Vall_hebron_NO2, aes(x = dt, y = value)) + 
+Vall_hebron_NO2_plt <- ggplot(Vall_hebron_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Vall Hebron")
 #Data only for 2010-2011??
 
 #Palau_reial
-ggplot(Palau_reial_NO2, aes(x = dt, y = value)) + 
+Palau_reial_NO2_plt <- ggplot(Palau_reial_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Palau Reial")
 #Data from 2011 to 2019
 #Observ_fabra
-ggplot(Observ_fabra_NO2, aes(x = dt, y = value)) + 
+Observ_fabra_NO2_plt <- ggplot(Observ_fabra_NO2, aes(x = dt, y = value)) + 
   geom_point(alpha = 0.5) +
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
   labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Observatori Fabra")
 #Data from 2018 to 2019
 
 #I'm going to discard Sagrera and St Gervasi from the study
 
 #Going to analyse the missing values by station
-summary(Poblenou_NO2)
-Poblenou_NO2_2019_03 <-Poblenou_NO2 %>% group_by(dt) %>% filter(year == 2019 & month==03 & (day ==14 & day == 15 & day = 16))
-View(Poblenou_NO2_2019_03)
-ggplot(Poblenou_NO2_2019_03, aes(x = dt, y = value)) + 
+
+#First I am going to plot a subset of data for Poblenou station:
+
+# Define Start and end times for the subset as POSICXct objects
+startTime <- as.POSIXct("2019-03-01 00:00:00",tz="UTC")
+endTime <- as.POSIXct("2019-03-8 00:00:00",tz="UTC")
+
+# create a start and end time R object
+start.end <- c(startTime,endTime)
+start.end
+
+#I have to format with time zone as otherwise ggplot2 doesnt deal with original date format
+date_format_tz <- function(format = "%Y-%m-%d", tz = "UTC") {
+  function(x) format(x, format, tz=tz)
+}
+
+Poblenou_NO2_subset_plt <- ggplot(Poblenou_NO2, aes(x = as.POSIXct(dt), y = value)) + 
   geom_line(alpha = 0.5) +
+  labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Poblenou NO2 Subset")+
   geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
-  labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Poblenou_NO2_2019")
+  coord_cartesian( ylim = c(0, 150))+
+  scale_x_datetime(limits=start.end,breaks='12 hours',labels = date_format_tz( "%d\n%H:%M", tz="UTC"))
+  
+
 #There are no measurements between 1am and 10am systematically, avoiding rush hour in the morning. 
-#I will focus on a week:
-ggplot(Poblenou_NO2_2019_03, aes(x = dt, y = value)) + 
-  geom_line(alpha = 0.5) +
-  geom_smooth(color = "grey", alpha = 0.2) +
-  scale_colour_gradientn(colours = terrain.colors(10)) +
-  scale_x_date(limit=c("2019-03-01","2019-03-11"))+
-  theme(legend.position = c(0.3, 0.9),
-        legend.background = element_rect(colour = "transparent", fill = NA), legend.direction = "horizontal")+
-  labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Poblenou_NO2_03_2019")
+# It seems there is a peak every morning around 9-10am, but we have no data to prove. 
+
+
+
+
 
