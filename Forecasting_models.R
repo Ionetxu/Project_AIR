78f0ba5314d18eb3825a6c4939bb9fa21f758389
Ione <ionelertxundi@hotmail.com>
2019-05-12 12:08:49 +0200

    Playing with models

--- Forecasting_models.R
+++ Forecasting_models.R
@@ -28,5 +28,62 @@ str(Eixample_ts )
 Eixample <- ts(Eixample_ts, start = c(2014, 1), frequency = 24)
 ##Let's plot the ts to see how it looks:
 autoplot(Eixample)
+summary(Eixample)
+
+train1 <- window(Eixample, end = c(2018, 11))
+#Let's do an autocorrelation plot to see the lag
+ggAcf(train1)
+
+#Augmented Dickey-Fuller Test
+adf.test(train1, alternative = "stationary")
+Pacf(train1)
+#No differencing required?
+
+#Let's try to decompose the data:
+decomp = stl(Eixample)
+autoplot(decomp)
+
+##NAIVE METHOD:
+fc1 <- snaive(train1, h = 24)
+accuracy(fc1, Eixample)
+autoplot(fc1)
+checkresiduals(fc1)
+
+##HOLT METHOD:
+fcholt <- holt(train1, h=24)
+# Look at fitted model using summary()
+summary(fcholt)
+# Plot the forecasts
+autoplot(fcholt)
+# Check that the residuals look like white noise
+checkresiduals(fcholt)
+accuracy(fcholt, Eixample)
+
+#Holt-Winters METHOD:
+fhw <- hw(train1, seasonal = "additive", h = 24)
+# Check if residuals look like white noise
+checkresiduals(fhw)
+autoplot(fhw)
+accuracy(fhw, Eixample)
+
+
+#Automatic forecasting with exponential smoothing - ETS model
+# Function to return ETS forecasts
+fitets <- ets(train1)
+checkresiduals(fitets)
+fitets %>% forecast(h = 24) %>% autoplot()
+
+#ARIMA
+fit <- auto.arima(train1)
+# Summarize the fitted model
+summary(fit)
+# Plot 2-year forecasts
+fit %>% forecast(h = 24) %>% autoplot()
+
+#ARIMA with FOURIER
+
+#ARIMA WITH WEATHER
+
+#TBATS model
 
 
