b7b3bbccd638fa98bc0dd24b9a543221515daae9
Ione <ionelertxundi@hotmail.com>
2019-05-20 12:56:51 +0200

    trends

--- Data_Exploration.R
+++ Data_Exploration.R
@@ -16,6 +16,8 @@ library(imputeTS)
 library(ggfortify)
 library(urca)
 library(forecast)
+library(hydroTSM)
+library(tidyquant)
 
 # Data Exploration
 
@@ -23,9 +25,46 @@ library(forecast)
 #questions I have about the data and I am going to try find the answers by using some additional datasets
 #like event calendar dates, weather and health data.
 
-#I will try to formulate questions or hypotesis and find answers.
-#1.What is the trend of the NO2 and PM10 pollutants in Eixample in the last 10 years? And in Gracia?
-#2. What is the trend of the NO2 and PM10 pollutants in Barcelona in the last 10 years?
+#I will try to formulate questions or hypotesis and find answers from the data.
+
+
+#1.What is the trend & seasonality of the NO2 and PM10 pollutants in Eixample in the last 10 years? And in Gracia?
+#Let's start by reading data of NO2 and PM10 pollutants in Eixample and Gracia.
+
+Eixample_NO2 <- read_csv('/Users/ione/Desktop/Project_AIR/data/Eixample_NO2_2014_2018.csv')
+head(Eixample_NO2)
+summary(Eixample_NO2)
+
+names(Eixample_NO2)[names(Eixample_NO2) == "imp_2014_2018_NO2_Eixample_intp"] <- "value_intp"
+summary(Eixample_NO2)
+
+#I am going to do the following analysis for NO2 in Eixample by using xts objects from zoo package.
+#Daily average - to see weekly seasonality
+#Monthly average --> yearly seasonality
+#Yearly average --> trend
+
+
+#Let's calculate the min, max and mean of each day:
+
+stat_fun <- function(x) c(min = min(x), max = max(x), mean = mean(x))
+Eixample_NO2_day <- Eixample_NO2 %>%
+  tq_transmute(select     = value_intp,
+               mutate_fun = apply.daily,
+               FUN        = stat_fun)
+summary(Eixample_NO2_day)
+
+# Use as.xts to generate an xts object of average daily NO2 data:
+Eixample_NO2_day_xts <- as.xts((Eixample_NO2_day[,-1]), order.by = as.Date(Eixample_NO2_day$dt))
+head(Eixample_NO2_day_xts)
+
+#Let's plot a subset of the data weekly:
+plot.zoo(Eixample_NO2_day_xts, plot.type = "single")
+
+
+
+
+
+#2. What is the trend of the NO2 and PM10 pollutants in Barcelona overall in the last 10 years?
 #3. What is the average yearly amount of NO2 in each station in Barcelona by year from 2014 to 2018? Does it pass the limit in any station?
 #4.What is the average yearly amount of PM10 in each station in Barcelona by year from 2014 to 2018? Does it pass the limit in any station?
 #5.How many times does NO2 pass the max hourly limit per year and per station?
@@ -34,15 +73,21 @@ library(forecast)
 #8.Is there any difference between weekday and weekend?
 #9.What is the month with higher average pollution NO2, PM10 during the year? And the minimum?
 #10.What is the most polluted station? And the least?
+
 #11.What is the relationship with weather and pollution? What is the component with more correlation?
 #12.What are the conditions with bad pollution in BCN? And conditions for good pollution?
+
 #13.Are hospitalizations with respiratory issues affected by peaks of pollution?
 #14.Are hospitalizations with heart issues affected by peaks of pollution?
-#15.How are public transpory strikes affecting to pollution?
+
+#15.How are public transport strikes affecting to pollution?
 #16.How are taxi strikes affecting to pollution?
+
 #17.Is a BCN football match affecting to pollution?
 #18.How is port activity influencing pollution?
+
 #19.How is air traffic influencing pollution?
+
 #20.How is bicing influencing pollution?
 
 
