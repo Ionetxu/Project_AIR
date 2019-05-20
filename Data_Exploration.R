9a6ef311fa93c04a92b797c3df3f477238ff15a8
Ione <ionelertxundi@hotmail.com>
2019-05-20 16:11:45 +0200

    general statistics

--- Data_Exploration.R
+++ Data_Exploration.R
@@ -58,14 +58,52 @@ Eixample_NO2_day_xts <- as.xts((Eixample_NO2_day[,-1]), order.by = as.Date(Eixam
 head(Eixample_NO2_day_xts)
 
 #Let's plot a subset of the data weekly:
-plot.zoo(Eixample_NO2_day_xts, plot.type = "single")
+plot.zoo(Eixample_NO2_day_xts["2018-01"], plot.type = "single", xy.labels = c("Time", "NO2_daily_values"))
+ggplot(Eixample_NO2_day, aes(x = dt, y = mean)) +
+  geom_line(alpha = 0.5) +
+  geom_smooth(color = "grey", alpha = 0.2) +
+  scale_x_datetime(breaks='1 year') +
+  labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2 (µg/m3) daily- Eixample")
 
+autoplot(Eixample_NO2_day_xts["2018-01"], facets = FALSE)
+#Not really very representative to see if there is a weekly seasonality. Let's analyze monthly data.
 
+Eixample_NO2_month <- Eixample_NO2 %>%
+  tq_transmute(select     = value_intp,
+               mutate_fun = apply.monthly,
+               FUN        = stat_fun)
+summary(Eixample_NO2_month)
+Eixample_NO2_month_xts <- as.xts((Eixample_NO2_month[,-1]), order.by = as.Date(Eixample_NO2_month$dt))
+summary(Eixample_NO2_month_xts)
+autoplot(Eixample_NO2_month_xts$mean)
 
+ggplot( aes(x = as.Date(dt), y = mean), data =Eixample_NO2_month ) +
+  geom_point(alpha = 0.5) +
+  labs( x = "Time", y = "NO2 (µg/m3)", title = "NO2(µg/m3) - Eixample NO2 monthly avg")+
+  geom_smooth(color = "grey", alpha = 0.2) +
+  scale_x_date(breaks='6 months')
+#I don't see a clear seasonality and the trend seems slightly positive, but not very clear trend from
+#2014 to 2019. Therefore the pollutant NO2 levels have not improved significantly since 2014,
+#since applying the strategic plan to fight pollution.
 
 
 #2. What is the trend of the NO2 and PM10 pollutants in Barcelona overall in the last 10 years?
+#See plots from before, they are slightly negative trend.
 #3. What is the average yearly amount of NO2 in each station in Barcelona by year from 2014 to 2018? Does it pass the limit in any station?
+
+Eixample_NO2_year <- Eixample_NO2 %>%
+  tq_transmute(select     = value_intp,
+               mutate_fun = apply.yearly,
+               FUN        = stat_fun)
+summary(Eixample_NO2_year)
+#We see that all values are over 40 µg/m3 which is the yearly average limit value recommended
+#by WHO and is followed by European Union recommendations.
+View(Eixample_NO2_year)
+ggplot(Eixample_NO2_year[-6,],aes(x=as.Date(dt),y=mean)) +
+  geom_line(alpha = 0.5) +
+  hline(inter)
+
+
 #4.What is the average yearly amount of PM10 in each station in Barcelona by year from 2014 to 2018? Does it pass the limit in any station?
 #5.How many times does NO2 pass the max hourly limit per year and per station?
 #6.How many times does PM10 pass the max hourly limit per year and per station?
